972a908e2ef5e9c90b978af6d76043518beffb54
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-07 08:59:46 -0600

    Curso C# Metodos I

---
+++ 010_Practica_Metodos/010_Practica_Metodos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _010_Practica_Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //mensajeEnPantalla();
+            //mensajeEnPantalla();
+            Console.WriteLine("Mensaje desde el main");
+            //mensajeEnPantalla();
+            //mensajeEnPantalla();
+            //mensajeEnPantalla();
+            SumarNumeros(7,9);
+        }
+        //static void mensajeEnPantalla()
+        //{
+        //    Console.WriteLine("Mensaje desde el metodo mensajeEnPantalla");
+        //}
+        static void SumarNumeros(int numero1, int numero2) 
+        {
+            Console.WriteLine($"La suma de { numero1 } y { numero2 } es { numero1 + numero2 }");
+        }
+
+    }
+}

36f73967ac2fcc2ec206f433d6a86d3a39e3f9fd
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-16 07:02:11 -0600

    Curso C# Video 14

---
+++ 014_CondicionalIF/014_CondicionalIF/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _014_CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool haceFrio;
+            //haceFrio = true;
+            //Console.WriteLine(!haceFrio);
+
+            int edad = 25;
+            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
+            if (edad >= 18) 
+            {
+                Console.WriteLine("Adelante, puedes pasar porque eres mayor de edad");
+            }
+        }
+    }
+}

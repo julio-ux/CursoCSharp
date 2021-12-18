e816372a23c92b0b3d9be26936e064a9b15e2e2a
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-18 13:36:24 -0600

    Video 15 Curso C#

--- 014_CondicionalIF/014_CondicionalIF/Program.cs
+++ 014_CondicionalIF/014_CondicionalIF/Program.cs
@@ -10,12 +10,33 @@ namespace _014_CondicionalIF
             //haceFrio = true;
             //Console.WriteLine(!haceFrio);
 
-            int edad = 25;
-            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
-            if (edad >= 18) 
+            //int edad = 25;
+            //Console.WriteLine("Vamos a evaluar si eres mayor de edad");
+            //if (edad >= 18)
+            //{
+            //    Console.WriteLine("Adelante, puedes pasar porque eres mayor de edad");
+            //    Console.WriteLine("Disfruta de tu instancia");
+            //}
+
+            Console.WriteLine("Vamos a evaluar si puedes conducir un vehiculo");
+            //bool carnet = false;
+            //if (carnet)
+            //    Console.WriteLine("Puedes conducir vehiculos");
+            //else
+            //    Console.WriteLine("No puedes conducir vehiculos");
+
+            //Console.WriteLine("Aquí termina el programa");
+
+            Console.WriteLine("Introduce tu edad, por favor");
+            int edad = Int32.Parse(Console.ReadLine());
+            if (edad >= 18)
             {
-                Console.WriteLine("Adelante, puedes pasar porque eres mayor de edad");
+                Console.WriteLine("¿Tienes carnet? si/no");
+                string carnet = Console.ReadLine();
+                if (carnet.ToLower() == "si")Console.WriteLine("Puedes conducir un vehiculo");
+                else Console.WriteLine("No puedes conducir un vehiculo");
             }
+            else Console.WriteLine("No puedes conducir un vehiculo");
         }
     }
 }

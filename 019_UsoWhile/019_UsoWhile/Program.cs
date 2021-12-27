9583a615effbd22656a54a40fc38d6045bd2e6fb
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-27 09:07:41 -0600

    Video 19 Bucle While

---
+++ 019_UsoWhile/019_UsoWhile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _019_UsoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("¿Deseas entrar en el buble While?");
+            //string respuesta = Console.ReadLine();
+            //while (respuesta != "no") 
+            //{
+            //    Console.WriteLine("Estás ejecutando el interior del bucle While");
+            //    Console.WriteLine("Introduce tu nombre, por favor");
+            //    string nombre = Console.ReadLine();
+            //    Console.WriteLine($"Saldrás del bucle {nombre} cunado respondas 'no' a la pregunta");
+            //    Console.WriteLine("¿Deseas repetir otra vez?");
+            //    respuesta = Console.ReadLine();
+            //}
+            //Console.WriteLine("Haz salido del bucle");
+            Random random = new Random();
+            int numero_random = random.Next(0,100);
+            int numero_dado = 0;
+            int intentos = 0;
+            do
+            {
+                intentos++;
+                Console.WriteLine("De un numero, por favor");
+                numero_dado = Int32.Parse(Console.ReadLine());
+                if (numero_dado > numero_random)
+                {
+                    Console.WriteLine("El numero debe ser menor");
+                }
+                else 
+                {
+                    Console.WriteLine("El numero debe ser mayor");
+                }
+            } while (numero_random != numero_dado);
+            Console.WriteLine($"Felicidades, lo logro en { intentos } intetntos");
+        }
+    }
+}

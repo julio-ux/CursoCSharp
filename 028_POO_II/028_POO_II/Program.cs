5c53c25b525a17a9640351f4ce2ea2bd3eac2ef9
julio-ux <72508490+julio-ux@users.noreply.github.com>
2022-01-23 07:51:26 -0600

    Curso C#. POO II. Creación de clases e instancias. Vídeo 28

---
+++ 028_POO_II/028_POO_II/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _028_POO_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo miCirculo = new Circulo();//Instanciar una clase 
+            //Instanciacion, Ejemplarizacion, Crear de ejemplar de clase
+                     
+            Console.WriteLine("El area del circulo es: " + miCirculo.calculoArea(5));
+
+            Circulo miCurculo2 = new Circulo();
+
+            Console.WriteLine("El area del circulo es: " + miCirculo.calculoArea(9));
+        }
+    }
+    class Circulo 
+    {
+        const double pi = 3.1416;//Propiedad de la clase Circulo. Campo de clase.
+        public double calculoArea(int radio)//Metodo de clase. ¿Que pueden hacer los objetos de tipo circulo? 
+        {
+            return pi * radio * radio;
+        }
+    }
+}

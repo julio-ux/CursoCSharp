ea7ea67e548b438aec276f8fc25fb95be56aa431
julio-ux <72508490+julio-ux@users.noreply.github.com>
2022-02-14 07:40:44 -0600

    Curso C#. POO VI. Visual Studio y POO. Vídeo 32

---
+++ 032_Conceptos_POO/032_Conceptos_POO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _032_Conceptos_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            realizarTarea();
+        }
+        static void realizarTarea() 
+        {
+            // TODO:
+            Punto origen = new Punto();
+            Punto destino = new Punto(125,80);
+
+        }
+    }
+}
---
+++ 032_Conceptos_POO/032_Conceptos_POO/Punto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _032_Conceptos_POO
+{
+    class Punto { 
+        public Punto() 
+        {
+            Console.WriteLine("Es es constructor por defecto");
+        }
+        public Punto(int x, int y) 
+        {
+            Console.WriteLine($"Coordenada x: {x}, Coordenada y: {y}");
+        }
+    }
+}

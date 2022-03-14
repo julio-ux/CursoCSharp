28ed338959ff08767b87d345e2f8944c53f13c8c
julio-ux <72508490+julio-ux@users.noreply.github.com>
2022-03-14 09:44:21 -0600

    Curso C#. Arrays I. Vídeo 36

---
+++ 036_UsoArrays_1/036_UsoArrays_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _036_UsoArrays_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] edades;
+            //edades = new int[4];
+
+            //edades[0] = 15;
+            //edades[1] = 27;
+            //edades[2] = 19;
+            //edades[3] = 80;
+
+            //Console.WriteLine(edades[2]);
+
+            int[] edades = {15,27,19,80 };
+            for (int i = 0; i < edades.Length; i++)
+            {
+                Console.WriteLine(edades[i]);
+            }
+        }
+    }
+}

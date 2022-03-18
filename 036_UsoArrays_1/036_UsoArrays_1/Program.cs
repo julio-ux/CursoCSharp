55cb9dd936c89767b48a08aa8790438800d36aad
julio-ux <72508490+julio-ux@users.noreply.github.com>
2022-03-18 11:37:26 -0600

    Curso C#. Arrays II. Vídeo 37

--- 036_UsoArrays_1/036_UsoArrays_1/Program.cs
+++ 036_UsoArrays_1/036_UsoArrays_1/Program.cs
@@ -17,10 +17,43 @@ namespace _036_UsoArrays_1
             //Console.WriteLine(edades[2]);
 
             int[] edades = {15,27,19,80 };
-            for (int i = 0; i < edades.Length; i++)
+            //for (int i = 0; i < edades.Length; i++)
+            //{
+            //    Console.WriteLine(edades[i]);
+            //}
+            //foreach (int edad in edades) 
+            //{
+            //    Console.WriteLine(edad);
+            //}
+
+            //array implicito
+            //var datos = new[] {"Juan", "Castillo", 15};
+            var valores = new[] {15,28,35,75.5,30.30};
+            //array de objetos
+            Empleados [] misempleados = new Empleados[2];
+            misempleados[0] = new Empleados("Julio",29);
+            misempleados[1] = new Empleados("Anna", 45);
+
+            foreach (Empleados e in misempleados) 
             {
-                Console.WriteLine(edades[i]);
+                Console.WriteLine(e.getInfoEmpleado());
             }
+            //Array de tipo o clases anonimas
+            var personas = new[] { new {Nombre = "Juan", Edad = 19}, new {Nombre = "Anna", Edad = 34}, new {Nombre = "Diana", Edad = 36} };
         }
+    }
+    class Empleados 
+    {
+        public Empleados(String nombre, int edad) 
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        public String getInfoEmpleado() 
+        {
+            return $"Nombre: {nombre}, edad: {edad}";
+        }
+        private String nombre;
+        private int edad;
     }
 }
